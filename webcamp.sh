d96a69c83ac7dad7b905aa8208dc9516b3dd0941
zhukovka <helen.zhukova@gmail.com>
2016-05-14 20:57:08 +0300

    rebuild css

--- webcamp.sh
+++ webcamp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-grunt  style -v
+grunt style -v
 cp /Users/lenka/webcampstyles/css/webcamp.min.css /Applications/MAMP/htdocs/newwebcamp/css
 cp /Users/lenka/webcampstyles/css/webcamp.min.css.map /Applications/MAMP/htdocs/newwebcamp/css
